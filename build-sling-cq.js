751401f72ac560a7447b27fd89739cfcb9f0e32c
watchcity <watchcity@users.noreply.github.com>
2015-01-02 06:13:29 -0500

    Update build-sling-cq.js

--- build-sling-cq.js
+++ build-sling-cq.js
@@ -3,83 +3,85 @@ var exec = require('child_process').exec;
 var path = require('path');
 var fs = require('fs');
 var util = require('util');
-
-//folder to copy jars to
-var cqinstallfolder = '/c/cq-build-jars'; // who puts spaces in folder names -- rename after reboot /publish/crx-quickstart/install/'
-
-//svn src folders for maven
+var net = require('net');
 
 var srcLocations = [],
   pkgNames = [],
-  i, command, d, mvnRun, cqSling;
+  i, command, d, mvnRun, cqSling,
+  readSettings, s, p, mvnJars;
 
-srcLocations[0] = './uhc_member/aarpmedicareplans';
-srcLocations[1] = './uhc_member/uhcmedicaresolutions';
-srcLocations[2] = './uhc_member/ui';
-srcLocations[3] = './uhc_acquisition/aarpmedicareplans'; //uhc-acquisition-aarpmedicareplans
-srcLocations[4] = './uhc_acquisition/ui'; //uhc-acquisition-ui
-srcLocations[5] = './uhc_acquisition/uhcmedicaresolutions'; //uhc-acquisition-uhcmedicaresolutions
-srcLocations[6] = './uhc_framework/ui'; //framework-ui
+// optionally read settings.json to get file paths, etc set to false to set inline
+readSettings = true;
 
-//package names for curl install
-pkgNames[0] = 'uhc-member-aarpmedicareplans';
-pkgNames[1] = 'uhc-member-uhcmedicaresolutions';
-pkgNames[2] = 'uhc-member-ui';
-pkgNames[3] = 'uhc-acquisition-aarpmedicareplans';
-pkgNames[4] = 'uhc-acquisition-ui';
-pkgNames[5] = 'uhc-acquisition-uhcmedicaresolutions';
-pkgNames[6] = 'framework-ui';
-
-
-var getTargetZip = function (i) {
-  var p = srcLocations[i] + '/target';
-  fs.readdir(p, function (err, list) {
-      if (err) {
-          throw err;
-      }
-      for (var l in list) {
-        console.log (list[l]);
-      }
-  });
-};
+if (readSettings && fs.existsSync('settings.json')) {
+  var settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
+  srcLocations = settings.srcLocations;
+  pkgNames = settings.pkgNames;
+  mvnJars = settings.mvnJars;
+  for (var i = 0; i<srcLocations.length; i++) {
+    console.log("srcLocation: " + srcLocations[i] + " | pkgName: " + pkgNames[i]);
+  }  
+} else if (!readSettings) {
+ //only set these if you didn't configure settings.json
+  srcLocations[0] = './uhc_member/aarpmedicareplans';
+  //... srcLocations[6] = './uhc_framework/ui';
+  pkgNames[0] = 'uhc-member-aarpmedicareplans';
+  //... pkgNames[6] = 'framework-ui';
+  mvnJars = 'yourArbitraryFolderForMvnToCopyJarsTo'
+}
 
 
+if (!fs.existsSync(mvnJars)) util.debug('no mvnJars');
+//see if publisher is running -- todo put ports/hosts etc in settings
+util.debug('about to connect to publisher');
+var client = net.connect({port: 4503},
+ function () {      
+  console.log('client connected');
+  client.end();
+});
+util.debug('about to connect to author');
+var client = net.connect({port: 4502},
+ function () {      
+  console.log('client connected');
+  client.end();
+});
+
+
+//
 var SlingPkg = function (location, pkgName) {
     this.location = location,
     this.mvnCommand = "mvn -f " + this.location + " clean install";
     this.srcLocation = this.location + "/target";
     this.pkgName = pkgName;
 
-    this.readDir = function (callback) {
+    this.readDir = function (sling) {
       var targetLocation = location + '/target';
       fs.readdir(targetLocation, function (err, list) {
         if (err) {
           throw err;
         }
         for (var l in list) {
-          //console.log('listl: ' + list[l]);
+          console.log('listl: ' + list[l]);
           if (list[l].indexOf('.zip') !== -1) {
-            console.log('found ZIP: ' + list[l]);
-            callback(list[l]);
+            util.debug('found ZIP: ' + list[l]);
+            sling(list[l]);
           } else if (list[l].indexOf('.jar') !== -1) {
-            console.log('found JAR: ' + list[l]);
+            util.debug('found JAR: ' + list[l]);
+            fs.createReadStream(targetLocation + '/' + list[l]).pipe(fs.createWriteStream(mvnJars + list[l]));
 
           }
-      //destPath = destPath.substring(path.dirname(destPath).indexOf('jcr_root/') + 9);
-        }
-        //  console.log ('THIS IS THE LIST! ' + list[l]);
-        
+        }        
       });
     };
 
-    this.mvnExec = function (callback1, callback2) {
-      console.log('mvnCommand: ' + this.mvnCommand);
-      exec(this.mvnCommand, function (error, stdout, stderr) {
+    this.mvnExec = function (read, sling) {
+      util.debug('mvnCommand: ' + this.mvnCommand);
+      exec(this.mvnCommand, {maxBuffer: 1024 * 500000}, function (error, stdout, stderr) {
         if (error !== null) {
           util.debug('exec error: ' + error);
         } else {
-          callback1(callback2);
-          console.log('Response: ' + stdout);
+          read(sling);
+          util.debug('Response: ' + stdout);
         };
       });
     }
@@ -90,22 +92,24 @@ var SlingPkg = function (location, pkgName) {
       slingCmd += " -F name="+pkgName+" -F force=true -F install=true";
       slingCmd += " http://localhost:4503/crx/packmgr/service.jsp";
 
-      console.log('slingCmd: ' + slingCmd);
-      exec(slingCmd, function (error, stdout, stderr) {
+      util.debug('slingCmd: ' + slingCmd);
+      exec(slingCmd, {maxBuffer: 1024 * 500000}, function (error, stdout, stderr) {
         if (error !== null) {
           util.debug('exec error: ' + error);
         } else {
-          console.log('Response: ' + stdout);
+          util.debug('Response: ' + stdout);
         };
       });
     };
   };
 
-var runAndGet = function () {
+var buildSlingCQ = function () {
   for (i = 0; i < srcLocations.length; i++) {
     var slingPkg = new SlingPkg(srcLocations[i], pkgNames[i]);
-    console.log('about to run mvnExec');
     var runExec = slingPkg.mvnExec(slingPkg.readDir, slingPkg.slingPkgToCQ);
-       console.log('ran mvnExec ' + i);
+       util.debug('started mvnExec ' + i);
   }
 }();
+
+
+//build-sling-cq.js
